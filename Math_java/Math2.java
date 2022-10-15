d95e61dde32a9c10fe9fc7942055b729a189c2a6
Std-Seo <101812506+Std-Seo@users.noreply.github.com>
2022-10-16 00:44:01 +0900

    Update Math2.java

--- Math_java/Math2.java
+++ Math_java/Math2.java
@@ -1,6 +1,6 @@
 public class math2 {
     public static void main(String[] args) {
-    // 3번 프린트 문제 7번 - 30 ~ 99 중 짝수의 개수 구하기
+    // 30 ~ 99 중 짝수의 개수 구하기
         int count = 0;
         for(int i = 30; i <= 99; i++) {
             if(i % 2 == 0) {
@@ -9,7 +9,7 @@ public class math2 {
         }
         System.out.println("짝수의 개수 : " + count + "가지");
 
-        // 3번 프린트 문제 8번 - 5의 배수의 개수 구하기
+        // 5의 배수의 개수 구하기
             int cnt = 0;
             for(int i = 20; i < 70; i++) {
                 if(i % 5 == 0) {
